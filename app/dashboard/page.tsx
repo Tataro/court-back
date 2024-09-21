1ff293cd08fdcf30980df417afba8c779e06afa1
Tataro <tat.kittitat@gmail.com>
2024-09-21 21:11:33 +0700

    show list

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,19 @@
 import { getUsers } from "./action";
 
 export default async function Page() {
-  const users = await getUsers();
+  const { users } = await getUsers();
 
   return (
     <div>
       <h1>Dashboard</h1>
-      <p>This is the dashboard page.</p>
+      <p>User List</p>
+      <hr />
+      {users.map((user) => (
+        <div key={user._id as string}>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
