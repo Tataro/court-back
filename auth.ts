3d90299e121f8dfe11908d7aa90788056d1d6b74
Tataro <tat.kittitat@gmail.com>
2024-09-23 00:22:35 +0700

    Refactor utility function and add login page
    - Refactor the `cn` utility function in `utils.ts` to use the `twMerge` function from `tailwind-merge` for better class merging.
    - Add a new login page component in `app/login/page.tsx` that renders a login form.

--- auth.ts
+++ auth.ts
@@ -3,4 +3,15 @@ import github from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [github],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl;
+      const isOnDashboard = pathname.startsWith("/dashboard");
+      if (pathname === "/" || isOnDashboard) return !!auth;
+      return true;
+    },
+  },
 });
